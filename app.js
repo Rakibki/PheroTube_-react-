cd3cc36e38d8e1da2f2e98b48a71b2583459644c
Rakib mia <rbepari404@gmail.com>
2023-09-01 04:08:30 -0600

    finished

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let temp;
 
 const getAllCategorys = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`)
@@ -12,8 +13,13 @@ const showAllCategorys = (data) => {
     data.forEach(item => {
         const btn = document.createElement('button')
         btn.classList.add("btn")
+        btn.classList.add("text-[#252525b3]")
+        btn.id ='categoryBtn'
         btn.innerHTML = `${item.category}`
-        btn.addEventListener('click', () => CategorysItems(item.category_id))
+        btn.addEventListener('click', () => {
+            temp = item.category_id;
+            CategorysItems(item.category_id)
+        })
         AllCategorys.appendChild(btn)
     });
 }
@@ -25,12 +31,11 @@ const CategorysItems = async (id) => {
     const allItemsContainer = document.getElementById('all-items-container')
     allItemsContainer.innerHTML = ""
 
-
     if(data.data.length === 0) {
         const div = document.createElement('div')
         allItemsContainer.classList.replace('grid', 'blobk')
         div.innerHTML = `
-                  <div class="mt-10">
+                  <div clfeass="mt-10">
                         <img class="mx-auto" src="images/Icon.png" alt="Shoes" />
                         <h1 class="text-center mt-6 font-semibold text-4xl">Oops!! Sorry, There is no <br> content here</h1>
                   </div>
@@ -40,18 +45,19 @@ const CategorysItems = async (id) => {
         allItemsContainer.classList.add('grid')
     }
 
-
-
-    // const sortData = data.data.sort((a, b) => {
-    //     return parseInt(a.others.views) - parseInt(b.others.views);
-    //     });
-    //     const rurrentData = sortData.reverse()
-       
-
     data.data.forEach((item) => {
+        function secondsToHms(d) {
+            var h = Math.floor(d / 3600);
+            var m = Math.floor(d % 3600 / 60);
+        
+            var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : "hrs, ") : "";
+            var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : "min") : "";
+            return hDisplay + mDisplay 
+        }
+
         const div = document.createElement('div')
         div.innerHTML = `
-            <div class="">
+            <div class="relative">
                 <figure><img class="rounded-md w-full h-48" src=${item.thumbnail} alt="Shoes" /></figure>
                     <div>
                     <div class="flex mt-4 items-center gap-2">
@@ -64,6 +70,7 @@ const CategorysItems = async (id) => {
                     </div>
                     <span class="text-[#171717b3] mt-3 text-sm font-medium">${item.others.views}</span>
                 </div>
+                ${item.others.posted_date ? `<span class="absolute bg-[#171717] text-white text-sm p-2 rounded-md font-medium right-2 bottom-32">${secondsToHms(item.others.posted_date)}</span>` : " "}
             </div>
         `
         allItemsContainer.appendChild(div)
@@ -71,10 +78,52 @@ const CategorysItems = async (id) => {
 }
 
 
-const handleSort = () => {
-    alert("cliked")
+
+const handleSort = async () => {
+        const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${temp}`)
+        const data = await res.json() 
+
+        const sortData = data.data.sort((a, b) => {
+            return parseInt(a.others.views) - parseInt(b.others.views);
+        });
+        const rurrentData = sortData.reverse()
+        console.log(rurrentData);        
+
+        const allItemsContainer = document.getElementById('all-items-container')
+        allItemsContainer.innerHTML = ""
+
+        function secondsToHms(d) {
+            var h = Math.floor(d / 3600);
+            var m = Math.floor(d % 3600 / 60);
+        
+            var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : "hrs, ") : "";
+            var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : "min") : "";
+            return hDisplay + mDisplay 
+        }
+
+        rurrentData.forEach((item) => {
+            const div = document.createElement('div')
+            div.innerHTML = `
+                <div class="relative">
+                    <figure><img class="rounded-md w-full h-48" src=${item.thumbnail} alt="Shoes" /></figure>
+                        <div>
+                        <div class="flex mt-4 items-center gap-2">
+                            <div><img class="w-12 h-12 rounded-full object-cover" src=${item.authors[0].profile_picture} alt="Shoes" /></div>
+                            <div><h3 class="text-xl font-semibold">${item.title}</h3></div>
+                        </div>
+                        <div class="flex mt-2 ml-6">
+                            <h2 class="text-[#171717b3] mr-2">${item.authors[0].profile_name}</h2>
+                            <img src="${item.authors[0].verified ? 'fi_10629607.svg' : ""}" /> 
+                        </div>
+                        <span class="text-[#171717b3] mt-3 text-sm font-medium">${item.others.views}</span>
+                    </div>
+                    <span class="absolute bg-[#171717] text-white text-sm p-2 rounded-md font-medium right-2 bottom-32">${secondsToHms(item.others.posted_date)}</span>
+                </div>
+            `
+            allItemsContainer.appendChild(div)
+        })
 }
 
 
-getAllCategorys()
+getAllCategorys(handleSort)
 CategorysItems(1000)
